d20484d23f8b0f80e678b93e3279b687f7b83b46
Jazilch <jzilch@hubspot.com>
2018-01-02 21:04:11 -0500

    finished auth flow

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+if (process.env.NODE_ENV === 'production') {
+	// we are in production
+	module.exports = require('./prod');
+} else {
+	// we are in dev
+	module.exports = require('./dev');
+}
--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require ('express');
-const app = express();
 const PORT = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
-});
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(
+	cookieSession({
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey]
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 
-app.listen(PORT, ()=> {
+
+require('./routes/authRoutes')(app);
+
+
+app.listen(PORT, () => {
 	console.log(`Server is running on ${PORT}`);
 });
