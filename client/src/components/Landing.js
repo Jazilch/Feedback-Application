5c37391c73ada6223e374709da4606751ba8ce71
Jazilch <jzilch@hubspot.com>
2018-02-25 21:10:47 -0500

    Updated CSS

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,44 +1,20 @@
 import React from 'react';
-
-const divStyle = {
-	backgroundImage: "url('https://cdn2.hubspot.net/hubfs/2676636/Tooltip/mountain-landscape-wallpaper-29048-29765-hd-wallpapers.jpg')",
-	backgroundSize: 'cover',
-	position: 'relative',
-	height: '100vh',
-	paddingBottom: '8%',
-	textAlign: 'center'
-};
-
-const overlayStyles = {
-	position: 'absolute',
-	top: '0',
-	left: '0',
-	bottom: '0',
-	right: '0',
-	backgroundColor: 'rgba(0,0,0, 0.5)'
-};
-
-const textWrapper = {
-	paddingTop: '10%'
-};
-const textStyles = {
-	color: '#fff',
-};
-
+import('./Landing.css');
 
 
 const Landing = () => {
 	return (
-		<div style={divStyle}>
-			<div style={overlayStyles}>
-				<div style={textWrapper}>
-					<h1 style={textStyles}>
+		<div className="hero">
+			<div className="hero_overlay">
+				<div className="content_wrapper">
+					<h1>
 						GetFeedback
 					</h1>
-					<p style={textStyles}>
-						Collect feedback from your users
-					</p>
-					<a class="waves-effect waves-light btn-large">Get Started</a>
+					<h5>
+						Getting feedback from your users is difficult but it doesn't have to be.
+						Quickly and easily start sending feddback surveys to get the feedback your app needs.
+					</h5>
+					<a href="/auth/google" class="waves-effect waves-light btn-large">Get Started</a>
 				</div>
 			</div>
 		</div>
